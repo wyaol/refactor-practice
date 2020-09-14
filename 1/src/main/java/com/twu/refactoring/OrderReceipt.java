439f1027bd2b6127099f3983bea363012c909069
wyao <289672494@qq.com>
2020-09-14 22:56:02 +0800

    [wang yao]:OrderReceipt refactoring

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -5,42 +5,34 @@ package com.twu.refactoring;
  * price and amount. It also calculates the sales tax @ 10% and prints as part
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
- * 
+ *
  */
 public class OrderReceipt {
-    private Order o;
+    private Order order;
 
-    public OrderReceipt(Order o) {
-        this.o = o;
+    public OrderReceipt(Order order) {
+        this.order = order;
 	}
 
 	public String printReceipt() {
 		StringBuilder output = new StringBuilder();
 
 		// print headers
-		output.append("======Printing Orders======\n");
-
-		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
+		String headers = "======Printing Orders======\n";
+		output.append(headers);
+		
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
 
 		// prints lineItems
 		double totSalesTx = 0d;
 		double tot = 0d;
-		for (LineItem lineItem : o.getLineItems()) {
-			output.append(lineItem.getDescription());
-			output.append('\t');
-			output.append(lineItem.getPrice());
-			output.append('\t');
-			output.append(lineItem.getQuantity());
-			output.append('\t');
-			output.append(lineItem.totalAmount());
-			output.append('\n');
+		for (LineItem lineItem : order.getLineItems()) {
+			apendLineItem(output, lineItem);
 
 			// calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * .10;
+			final double rate = .10;
+			double salesTax = lineItem.totalAmount() * rate;
             totSalesTx += salesTax;
 
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
@@ -53,5 +45,16 @@ public class OrderReceipt {
         // print total amount
 		output.append("Total Amount").append('\t').append(tot);
 		return output.toString();
+	}
+
+	private void apendLineItem(StringBuilder output, LineItem lineItem) {
+		output.append(lineItem.getDescription());
+		output.append('\t');
+		output.append(lineItem.getPrice());
+		output.append('\t');
+		output.append(lineItem.getQuantity());
+		output.append('\t');
+		output.append(lineItem.totalAmount());
+		output.append('\n');
 	}
 }
