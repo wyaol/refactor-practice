d0ed28ef820d290b358ae016cc6d1cd5be7569b7
wyao <289672494@qq.com>
2020-09-14 23:21:22 +0800

    [wang yao]:NumberCruncher rafectoring

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,5 +1,7 @@
 package com.twu.refactoring;
 
+import java.net.InterfaceAddress;
+
 public class NumberCruncher {
     private final int[] numbers;
 
@@ -8,34 +10,30 @@ public class NumberCruncher {
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return getCount(number -> number % 2 ==0);
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return getCount(number -> number % 2 ==1);
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
+        return getCount(number -> number > 0);
     }
 
     public int countNegative() {
+        return getCount(number -> number < 0);
+    }
+
+    private int getCount(Counter counter) {
         int count = 0;
         for (int number : numbers) {
-            if (number < 0) count++;
+            if (counter.count(number)) count++;
         }
         return count;
     }
+}
+
+interface Counter {
+    public Boolean count(int number);
 }
